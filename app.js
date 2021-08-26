6aa48242d085253553777648c2e9f523edaa636f
josue Micaroni do Rosario <josuemrosario@yahoo.com.br>
2021-08-26 14:47:21 -0300

    Aula 14 NPM Instalndo o EJS

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req,res){
-
-	res.send("<html><body>Portal de Noticias</body><html>");
-});
+app.set('view engine','ejs');
 
 app.get('/tecnologia',function(req,res){
+	res.render("secao/tecnologia");
+});
 
-	res.send("<html><body>Noticias de tecnologia</body><html>");
+app.get('/',function(req,res){
+	res.send("<html><body>Portal de Noticias</body><html>");
 });
 
 app.listen(3000, function(){
