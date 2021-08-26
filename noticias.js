414bbc3afd3527ccf07f10efa7ed2eda1990a2ec
josue Micaroni do Rosario <josuemrosario@yahoo.com.br>
2021-08-26 14:14:21 -0300

    Aula 9 Respondendo requisicoes com base na url requisitada

--- noticias.js
+++ noticias.js
@@ -2,7 +2,27 @@ var http = require('http');
 
 var server = http.createServer(function(req, res){
 
-	res.end("<html><body>Portal de noticias</body><html>");
+	var categoria = req.url;
+
+	
+	if(categoria == 'tecnologia'){
+
+		res.end("<html><body>Noticias de Tecnologia</body><html>");
+	
+	}else if (categoria == '/moda'){
+
+		res.end("<html><body>Noticias de Moda</body><html>");
+
+	}else if (categoria == '/beleza'){
+
+		res.end("<html><body>Noticias de Beleza</body><html>");
+
+	}else{
+
+		res.end("<html><body>Portal de Noticias</body><html>");
+	}
+
+	
 
 });
 
