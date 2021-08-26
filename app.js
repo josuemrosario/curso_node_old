bb7d3409c920885e2677e87764aeeb0529ba6163
josue Micaroni do Rosario <josuemrosario@yahoo.com.br>
2021-08-26 14:59:05 -0300

    Aula 16. Refactoring do projeto prático portal de noticias com EJS

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ var app = express();
 
 app.set('view engine','ejs');
 
-app.get('/tecnologia',function(req,res){
-	res.render("secao/tecnologia");
+app.get('/',function(req,res){
+	res.render("home/index.ejs");
 });
 
-app.get('/',function(req,res){
-	res.send("<html><body>Portal de Noticias</body><html>");
+app.get('/formulario_inclusao_noticia',function(req,res){
+	res.render("admin/form_add_noticia.ejs");
 });
+
+app.get('/noticias',function(req,res){
+	res.render("noticias/noticias.ejs");
+});
+
 
 app.listen(3000, function(){
 	console.log("Servidor Rodando com Express");
