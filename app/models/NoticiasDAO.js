4b134457755fabbb94b20429f376facc532a5a5a
josue Micaroni do Rosario <josuemrosario@yahoo.com.br>
2022-01-24 15:10:56 -0300

    secao 10 Ganhando produtividade com express

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -13,6 +13,7 @@ NoticiasDAO.prototype.getNoticia = function(callback){
 }
 
 NoticiasDAO.prototype.salvarNoticia = function(noticia, callback){
+		console.log(noticia);
 		this._connection.query('insert into noticias set ?',noticia,callback);
 }
 
